119df03fd83f04a85a6d43a87aefd3e03b6f2994
unknown <markchck@gmail.com>
2022-09-25 21:43:16 +0900

    aa

--- 22. 9020.py
+++ 22. 9020.py
@@ -5,17 +5,28 @@
 #     3.1 그 소수를 2번 더해서 10이 나오나 확인하고 나오면 그 놈이 골디바흐
 #     3.2 (아니라면) 그 소수를 10과 빼고 나머지 값이 다른 소수리스트에 잇는지 확인한다. 그놈이 골디바흐임.
 
+
+
+# number(ex. 10)보다 작은 소수들을 배열로 뱉는 함수를 만들 것.
+# 문제 조건 상 number는 2보다 큰 짝수임.
+def select_p_number(number): #number>2
+  prime_number_list = []
+  for n in range(2, number):
+    # n이 소수인지 판별해야함.
+    if n ==2:
+      prime_number_list.append(n)
+    elif n%2==0:
+      None
+    else:
+      # n을 n보다 작은 수로 나눴을 때 나머지가 0이되면 소수 아님.
+      for itr_n in n:
+        if n%itr_n == 0:
+          None
+        else:
+          prime_number_list.append(n)
+  return prime_number_list
+
 Times = int(input())
 for t in range(Times):
   number = int(input())
-  # select_p_number(number)
-
-# number(ex. 10)보다 작은 소수들을 배열로 뱉는 함수를 만들 것.
-def select_p_number(number):
-  if number==1:
-    None
-  elif number ==2:
-    return number
-  elif number
-  for n in range(2, number):
-    n
+  print(select_p_number(number))
