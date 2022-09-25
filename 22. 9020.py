79d5e8723811992ae5fd9747434d1bb0904759d4
unknown <markchck@gmail.com>
2022-09-25 23:35:41 +0900

    aa

--- 22. 9020.py
+++ 22. 9020.py
@@ -1,10 +1,11 @@
 # https://www.acmicpc.net/problem/9020
 # 1. 소수를 구하는 함수를 만든다 0
-# 2.10이 주어지면 10보다 작은 소수를 모두 구하고 0
-# 3 그 소수들 중 하나씩  
+# 2. 10이 주어지면 10보다 작은 소수를 모두 구하고 0
+# 3. 그 소수들 중 하나씩  
 #     3.1 그 소수를 2번 더해서 10이 나오나 확인하고 나오면 그 놈이 골디바흐
 #     3.2 (아니라면) 그 소수를 10과 빼고 나머지 값이 다른 소수리스트에 잇는지 확인한다. 그놈이 골디바흐임.
-
+# 4. 출력될 때 3,7만 출력되고 7,3은 안되게 하는 코드
+# 5. 소수의 차이가 덜 나는 걸 먼저 뽑아내는 코드
 
 
 # number(ex. 10)보다 작은 소수들을 배열로 뱉는 함수를 만들 것.
@@ -35,13 +36,28 @@ def return_p_number_list(number): #문제조건 상 number>2
   return out_put_list
 
 Times = int(input())
+input_list=[]
 for t in range(Times):
   number = int(input())
+  input_list.append(number)
+
   goldi = []
   for i in return_p_number_list(number):
     if i+i == number:
       goldi.append([i,i])
+      # break
     else:
-      if (number - i) in return_p_number_list:
+      if (number - i) in return_p_number_list(number):
         goldi.append([i, number-i])
-print(goldi)
+  
+  # 4
+  for itr_goldi in goldi:
+    if (itr_goldi[0] > itr_goldi[1]):
+      del(itr_goldi)
+    elif(itr_goldi[0] == itr_goldi[1]):
+        first_output = itr_goldi[0]
+        second_output = itr_goldi[1]
+    else:
+        first_output = itr_goldi[0]
+        second_output = itr_goldi[1]
+  print(first_output, second_output)
