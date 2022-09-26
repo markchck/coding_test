85871a809f710f655757f067188cd0e79dc83792
markchck <markchck@gmail.com>
2022-09-27 08:56:02 +0900

    s

--- 번외편) 재귀 이해용.py
+++ 번외편) 재귀 이해용.py
@@ -1,3 +1,4 @@
+# 내코드 뭔가 이상함.
 def a(n):
   if (n == 0):
     return ("")
@@ -6,6 +7,8 @@ def a(n):
     print("#"*(i+1))
 print(a(2))
 
+
+# 종우님 코드
 # def a(n):
 #   if (n == 0):
 #     return ("")
