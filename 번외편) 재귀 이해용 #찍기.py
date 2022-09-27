106aacbd80c811bcc2a6db0f5dec4010796bc365
markchck <markchck@gmail.com>
2022-09-27 16:13:51 +0900

    aa

---
+++ 번외편) 재귀 이해용 #찍기.py
@@ -0,0 +1,17 @@
+# 내코드 뭔가 이상함.
+# def a(n):
+#   if (n == 0):
+#     return ("")
+#   a(n-1)
+#   for i in range(n):
+#     print("#"*(i+1))
+# print(a(2))
+
+
+# 종우님 코드
+def a(n):
+  if (n == 0):
+    return ("")
+  a(n-1)
+  print("#"*n)
+a(4)
