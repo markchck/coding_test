43f410a922f3ebe26abdab8009bc52b7a924727d
markchck <markchck@gmail.com>
2023-03-25 17:45:42 +0900

    10950

--- javascript/10950.js
+++ javascript/10950.js
@@ -1,4 +1,4 @@
-let input = require("fs").readFileSync('example.txt').toString().split('\n');
+let input = require("fs").readFileSync('/dev/stdin').toString().split('\n');
 for (let i=1; i<=input[0]; i++){
     let numbers = input[i].split(' ');
     let elements=numbers.map(function(elem){
