6a1dab1c9c5cafd2ab92661b687ad16ec6380537
markchck <markchck@gmail.com>
2023-09-01 12:18:52 +0900

    종이자르기

--- 연습장.py
+++ 연습장.py
@@ -1,11 +1,42 @@
-import random
+def solution(arr):
+    N = len(arr)
+    first = arr[0][0]
+    red, blue = 0, 0
+    flag = True
+    for r in range(N):
+        for c in range(N):
+            if arr[r][c] != first:
+                flag = False
+                break
+
+    if flag == True:
+        if first == 0:
+            red += 1
+        else:
+            blue += 1
+    else:  # 자르기 들어가야함.
+        newN = N//2
+        # arr1, arr2, arr3, arr4 = [], [], [], []
+        arr1 = [[0] * newN for _ in range(newN)]
+        arr2 = [[0] * newN for _ in range(newN)]
+        arr3 = [[0] * newN for _ in range(newN)]
+        arr4 = [[0] * newN for _ in range(newN)]
+
+        for r in range(N):
+            for c in range(N):
+                if 0 <= r < newN and 0 <= c < newN:
+                    arr1[r % newN][c % newN] = (arr[r][c])
+                elif 0 <= r < newN and newN <= c < N:
+                    arr2[r % newN][c % newN] = (arr[r][c])
+                elif newN <= r < N and 0 <= c < newN:
+                    arr3[r % newN][c % newN] = (arr[r][c])
+                else:
+                    arr4[r % newN][c % newN] = (arr[r][c])
+        solution(arr1)
+        solution(arr2)
+        solution(arr3)
+        solution(arr4)
+    return [red, blue]
 
 
-def solution(number, k):
-    digit = 1000000
-    lower = 4300
-    ran = str(random.randint(lower, lower+1))
-    print(ran)
-
-
-solution(1, 2)
+solution([[1, 1, 0, 0], [1, 0, 0, 0], [1, 0, 0, 1], [1, 1, 1, 1]])
---
+++ 연습장2.py
@@ -0,0 +1,9 @@
+# a = list(3)
+# print(a)
+
+
+a = [1]
+a[0] = 2
+# arr1 = [[0 for _ in range(2)] for _ in range(2)]
+arr1 = [[0] * 2 for _ in range(2)]
+print(arr1)
