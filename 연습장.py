2256ada97e0003d0a2efba5e312a285643b89c08
markchck <markchck@gmail.com>
2023-09-04 10:29:02 +0900

    파일명 정렬

--- 연습장.py
+++ 연습장.py
@@ -1,42 +1,21 @@
-def solution(arr):
-    N = len(arr)
-    first = arr[0][0]
-    red, blue = 0, 0
-    flag = True
-    for r in range(N):
-        for c in range(N):
-            if arr[r][c] != first:
-                flag = False
+def solution(files):
+    tmp = []
+    head, number, tail = '', '', ''
+
+    for file in files:
+        for i in range(len(file)):
+            if file[i].isdigit():
+                head = file[:i]
+                number = file[i:]
+                for j in range(len(number)):
+                    if not number[j].isdigit():
+                        tail = number[j:]
+                        number = number[:j]
+                        break
+                tmp.append([head, number, tail])
+                head, number, tail = '', '', ''
                 break
+    tmp = sorted(tmp, key=lambda x: (x[0].lower(), x[1]))
 
-    if flag == True:
-        if first == 0:
-            red += 1
-        else:
-            blue += 1
-    else:  # 자르기 들어가야함.
-        newN = N//2
-        # arr1, arr2, arr3, arr4 = [], [], [], []
-        arr1 = [[0] * newN for _ in range(newN)]
-        arr2 = [[0] * newN for _ in range(newN)]
-        arr3 = [[0] * newN for _ in range(newN)]
-        arr4 = [[0] * newN for _ in range(newN)]
-
-        for r in range(N):
-            for c in range(N):
-                if 0 <= r < newN and 0 <= c < newN:
-                    arr1[r % newN][c % newN] = (arr[r][c])
-                elif 0 <= r < newN and newN <= c < N:
-                    arr2[r % newN][c % newN] = (arr[r][c])
-                elif newN <= r < N and 0 <= c < newN:
-                    arr3[r % newN][c % newN] = (arr[r][c])
-                else:
-                    arr4[r % newN][c % newN] = (arr[r][c])
-        solution(arr1)
-        solution(arr2)
-        solution(arr3)
-        solution(arr4)
-    return [red, blue]
-
-
-solution([[1, 1, 0, 0], [1, 0, 0, 0], [1, 0, 0, 1], [1, 1, 1, 1]])
+    for i in tmp:
+        print(i)
--- 연습장2.py
+++ 연습장2.py
@@ -1,9 +1,2 @@
-# a = list(3)
-# print(a)
-
-
-a = [1]
-a[0] = 2
-# arr1 = [[0 for _ in range(2)] for _ in range(2)]
-arr1 = [[0] * 2 for _ in range(2)]
-print(arr1)
+items = 1
+print(type(items) == str)
