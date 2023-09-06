7a609ea9cb8ae35ae189a2cfcb0abde7597d493b
markchck <markchck@gmail.com>
2023-09-06 22:23:09 +0900

    크게만들기

--- 연습장.py
+++ 연습장.py
@@ -1,20 +1,23 @@
-# n부터 왼쪽으로 키를 재면 n-1번 봐야하기 때문에 N^2이 됨
-# 오른쪽으로 진행하면서 스택의 top보다 현재 숫자가 크면 넣고 스택 팝하고, 작으면 현재 숫자 append한다.
-# 이렇게하면 그림자에 가려지는 놈들 9 5 7에서 5같은 놈들을 보지 않아도 되기 때문에 시간 단축된다.
-
 import sys
 input = sys.stdin.readline
-N = int(input())
-items = list(map(int, input().split()))
+N, K = map(int, input().split())
+items = str(input().strip())
+items = list(items)
 stack = []
-answer = [0] * N
+count = 0
 for i, item in enumerate(items):
-    while stack:
-        lastIndex, lastItem = stack[-1]
-        if lastItem < item:
+    while stack and count != K:
+        lastItem = stack[-1]
+        if item > lastItem:
             stack.pop()
+            count += 1
         else:
-            answer[i] = lastIndex
             break
-    stack.append((i+1, item))
-print(*answer)
+    stack.append(item)
+print(stack)
+if len(stack) != N-K:
+    for i in range(K):
+        stack.pop()
+# while len(stack) > N-K:
+#     stack.pop()
+print(''.join(stack))
--- 연습장2.py
+++ 연습장2.py
@@ -1,34 +1,17 @@
-def solution(dice):
-    length = len(dice)
-    items = []
-
-    for i, case in enumerate(dice):
-        A = set((0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
-        B = set(case)
-        items.append((A-B))
-
-    temps = [0] * 10
-    for item in items:
-        for i in item:
-            temps[i] += 1
-
-    result = []
-    for i in range(1, len(temps)):
-        if temps[i] >= (length / 2):
-            count = 0
-            for case in dice:
-                if i not in case:
-                    count += 1
-
-            if length % 2 == 0:
-                result.append(str(i) * (count + 1))
-            else:
-                result.append(str(i) * (count))
-            break
-    print(temps)
-    for re in result:
-        print(re)
-        return int(re)
-
-
-solution([[1, 2, 4, 5, 6, 8], [0, 3, 9, 5, 6], [1, 6, 8, 9, 3]])
+import sys
+input = sys.stdin.readline
+N, K = map(int, input().split())
+items = str(input().strip())
+items = list(items)
+stack = []
+count = 0
+k = K
+for i, item in enumerate(items):
+    while stack and k > 0 and stack[-1] < items[i]:
+        stack.pop()
+        k -= 1
+    stack.append(item)
+# if len(stack) != N-K:
+#     for i in range(K):
+#         stack.pop()
+print("".join(map(str, stack[: N-K])))
