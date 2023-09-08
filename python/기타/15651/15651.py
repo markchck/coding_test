8db936f2ed8b6635cb90f79c09f6afb0d4a38f21
markchck <markchck@gmail.com>
2023-09-08 09:38:15 +0900

    백트레킹

--- python/기타/15651/15651.py
+++ python/기타/15651/15651.py
@@ -1,9 +1,19 @@
-# permutation은 한번 뽑은 숫자는 다시 못 쓴다는 제약이 있기 때문에 이 문제에선 쓸 수가 없다.
+# permutation은 한번 뽑은 숫자는 다시 못 쓴다는 제약이 있기 때문에 이 문제처럼 여번 써야하는 상황에선 쓸 수가 없다.
 import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
+
+
+def dfs():
+    if len(items) == M:
+        print(*items)
+        return
+    else:
+        for i in range(1, N+1):
+            items.append(i)
+            dfs()
+            items.pop()
+
+
 items = []
-for i in range(M):
-    answer = [0] * M
-    for j in range(1, N+1):
-        answer[i] = j
+dfs()
