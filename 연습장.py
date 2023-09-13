50b2658fd4a853e1acf3ec3de8cd523f94f70d4b
markchck <markchck@gmail.com>
2023-09-13 11:55:30 +0900

    tmax

--- 연습장.py
+++ 연습장.py
@@ -1,28 +1,27 @@
+
 from collections import deque
 
 
-def solution(maps):
-    N = len(maps)
-    M = len(maps[0])
-    que = deque()
-    dr = [-1, 1, 0, 0]
-    dc = [0, 0, 1, -1]
-    visited = [[0] * (M) for _ in range(N)]
-    que.append((0, 0))
-    visited[0][0] = 1
-
-    while que:
-        r, c = que.popleft()
-        if r == N-1 and c == M-1:
-            return visited[r][c]
-        for i in range(4):
-            new_r = r + dr[i]
-            new_c = c + dc[i]
-            if 0 <= new_r < N and 0 <= new_c < M and maps[new_r][new_c] == 1 and visited[new_r][new_c] == 0:
-                visited[new_r][new_c] = visited[r][c] + 1
-                que.append((new_r, new_c))
+def solution(dice):
+    i = 0
+    while i < 9999:
+        number = i
+        Ndice = dice[:]
+        i = deque(list(str(i)))
+        while i:
+            now = int(i.popleft())
+            for idx, case in enumerate(Ndice):
+                if now in case:
+                    del Ndice[idx]
+                    break
+            else:
+                print(number)
+                return number
+        i = number + 1
+    print(-1)
     return -1
 
 
-solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [
-         1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]])
+# solution([[1, 2, 3, 4, 6, 7, 5], [0, 6, 4, 3, 6, 0, 9], [1, 2, 0, 5, 6, 7, 8]])
+solution([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [
+         0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]])
--- 연습장2.py
+++ 연습장2.py
@@ -1,17 +1,3 @@
-import sys
-input = sys.stdin.readline
-N, K = map(int, input().split())
-items = str(input().strip())
-items = list(items)
-stack = []
-count = 0
-k = K
-for i, item in enumerate(items):
-    while stack and k > 0 and stack[-1] < items[i]:
-        stack.pop()
-        k -= 1
-    stack.append(item)
-# if len(stack) != N-K:
-#     for i in range(K):
-#         stack.pop()
-print("".join(map(str, stack[: N-K])))
+a = [1]
+if '1' in a:
+    print(1)
